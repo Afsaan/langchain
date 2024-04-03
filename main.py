00a58a849a779ebcf2275c722045c7780f408fbc
Afsan Khan <afsankhan@Afsans-MacBook-Air.local>
2024-04-04 00:05:50 +0530

    phase 2

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 from constant import openai_key
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
+from langchain.chains import LLMChain
+
+from langchain.chains import SimpleSequentialChain
 
 import streamlit as st
 
@@ -16,11 +19,23 @@ input_text = st.text_input("Search the topic you want")
 # prompt template
 
 first_input_prompt = PromptTemplate(
-    input_variables = ["topic"]
+    input_variables = ["name"],
+    template = "Tell me about celebrity {name}"
 )
 
 ## OPENAI LLMS
 llm = OpenAI(temperature=0.8) #how unique the output will be (0- not unique and 1- unique data)
+chain = LLMChain(llm=llm, prompt=first_input_prompt, verbose=True, output_key="person")
+
+second_input_prompt = PromptTemplate(
+    input_variables = ["person"],
+    template = "when {person} was born"
+)
+
+chain2 = LLMChain(llm=llm, prompt=second_input_prompt, verbose=True, output_key="dob")
+
+# combining both the chain and set a seq
+parent_chain = SimpleSequentialChain(chains=[chain, chain2], verbose=True)
 
 if input_text:
-    st.write(llm(input_text))
+    st.write(parent_chain.run(input_text))
