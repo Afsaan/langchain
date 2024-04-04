b9030116dbcaffbd7683ea1f89f318595363bc62
Afsan Khan <afsankhan@Afsans-MacBook-Air.local>
2024-04-04 07:23:21 +0530

    phase 3

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
 
-from langchain.chains import SimpleSequentialChain
+from langchain.memory import ConversationBufferMemory
+
+from langchain.chains import SequentialChain
 
 import streamlit as st
 
@@ -29,13 +31,30 @@ chain = LLMChain(llm=llm, prompt=first_input_prompt, verbose=True, output_key="p
 
 second_input_prompt = PromptTemplate(
     input_variables = ["person"],
-    template = "when {person} was born"
+    template = """
+                when
+                {person} 
+                was born . i want to output has date month year only! no intrested in any other output"""
 )
 
 chain2 = LLMChain(llm=llm, prompt=second_input_prompt, verbose=True, output_key="dob")
 
-# combining both the chain and set a seq
-parent_chain = SimpleSequentialChain(chains=[chain, chain2], verbose=True)
+third_input_prompt = PromptTemplate(
+    input_variables = ["dob"],
+    template = """tell me any 5 major events that happend around {dob}
+                and it should be in points"""
+)
 
+chain3 = LLMChain(llm=llm, prompt=third_input_prompt, verbose=True, output_key="event")
+
+# combining both the chain and set a seq
+parent_chain = SequentialChain(chains=[chain, chain2, chain3], input_variables=["name"], output_variables=["person", "dob", "event"], verbose=True)
+
+#memeory
+person_memeory = ConversationBufferMemory(input_key="person", memory_key='chat_history')
+dob_memeory = ConversationBufferMemory(input_key="person", memory_key='chat_history')
+
+
+# we need to give the value in key value pair
 if input_text:
-    st.write(parent_chain.run(input_text))
+    st.write(parent_chain({"name" : input_text}))
